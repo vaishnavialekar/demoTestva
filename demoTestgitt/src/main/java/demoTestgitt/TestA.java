c931d47ca8995dcadba03c2a18be213829ea5a74
vaishnavi alekar <vaishnavialekar@rediffmail.com>
2023-05-06 13:06:11 +0530

    second changes

---
+++ demoTestgitt/src/main/java/demoTestgitt/TestA.java
@@ -0,0 +1,11 @@
+package demoTestgitt;
+
+public class TestA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("class b"+"hasgj"
+			);
+	}
+
+}
